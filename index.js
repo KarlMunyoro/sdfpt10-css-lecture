0978b9191c94d2a010f9cc6e7fe320d9b09f08b6
Karl Munyoro Kamau <kxrlmunyoro@gmail.com>
2025-03-28 18:15:22 +0300

    Made some changes

--- index.js
+++ index.js
@@ -23,3 +23,64 @@ imageContainer.style.backgroundColor = 'lightblue';
 
 const courseoutcome=document.getElementById("course-outcomes")
 courseoutcome.style.color="gold";
+
+let newrow=document.createElement("tr");
+let tablebody=document.querySelector("tbody");
+
+let number=document.createElement("td");
+number.textContent="3";
+newrow.append(number);
+
+let date1=document.createElement("td");
+date1.textContent="06-February-2025"
+newrow.append(date1);
+
+let date2=document.createElement("td");
+date2.textContent="26-September-2025";
+newrow.append(date2);
+
+let date3=document.createElement("td");
+date3.textContent="11-November-2025";
+newrow.append(date3);
+
+let date4=document.createElement("td");
+date4.textContent="10-October-2025";
+newrow.append(date4);
+
+tablebody.append(newrow);
+
+let newrow2=document.createElement("tr");
+
+let number2=document.createElement("td");
+number2.textContent="4";
+newrow2.append(number2);
+
+let q1=document.createElement("td");
+q1.textContent="06-February=2026";
+newrow2.append(q1);
+
+let q2=document.createElement("td");
+q2.textContent="13-November-2026";
+newrow2.append(q2);
+
+let q3=document.createElement("td");
+q3.textContent="19-May-2026";
+newrow2.append(q3);
+
+let q4=document.createElement("td");
+q4.textContent="29-January-2025";
+newrow2.append(q4);
+
+tablebody.append(newrow2);
+
+let submitbtn=document.querySelector("input[type='submit']")
+
+submitbtn.addEventListener('click', (event)=> {
+    event.preventDefault();
+    let email=document.querySelector("input[type='email']").value
+    let tel=document.querySelector("input[type='tel']").value
+    let usernumber=document.querySelector("input[type='number']").value
+    let text=document.querySelector("input[type='text']").value
+})
+
+
